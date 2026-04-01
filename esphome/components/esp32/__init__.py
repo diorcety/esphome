2e98c785195a376cd53139920a7152b49d88d6b2
Yann Diorcet <diorcet.yann@gmail.com>
2026-04-01 23:21:30 +0200

    aduino support

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -432,6 +432,9 @@ def set_core_data(config):
     if conf[CONF_TYPE] == FRAMEWORK_ESP_IDF:
         CORE.data[KEY_ESP32][KEY_IDF_VERSION] = framework_ver
     elif (idf_ver := ARDUINO_IDF_VERSION_LOOKUP.get(framework_ver)) is not None:
+        if CORE.using_native_idf:
+            # Official ESP-IDF frameworks don't use extra
+            idf_ver = cv.Version(idf_ver.major, idf_ver.minor, idf_ver.patch)
         CORE.data[KEY_ESP32][KEY_IDF_VERSION] = idf_ver
     else:
         raise cv.Invalid(
@@ -796,14 +799,17 @@ def _check_pio_versions(config):
 
 
 def _check_native_idf_versions(config):
-    config = config.copy()
+    config = _check_pio_versions(config)
     value = config[CONF_FRAMEWORK]
-    if value[CONF_VERSION] in ESP_IDF_FRAMEWORK_VERSION_LOOKUP:
-        version = ESP_IDF_FRAMEWORK_VERSION_LOOKUP[value[CONF_VERSION]]
-    else:
-        version = cv.Version.parse(cv.version_number(value[CONF_VERSION]))
+
+    if CONF_SOURCE in value:
+        del value[CONF_SOURCE]
+
+    if CONF_PLATFORM_VERSION in value:
+        del value[CONF_PLATFORM_VERSION]
 
     # Official ESP-IDF frameworks don't use extra
+    version = cv.Version.parse(value[CONF_VERSION])
     version = cv.Version(version.major, version.minor, version.patch)
 
     value[CONF_VERSION] = str(version)
@@ -2278,9 +2284,19 @@ def _write_idf_component_yml():
             if stub_path.exists():
                 rmtree(stub_path)
 
+        if CORE.using_native_idf:
+            add_idf_component(
+                name="espressif/arduino-esp32",
+                repo="https://github.com/diorcety/arduino-esp32.git",
+                ref=str(CORE.data[KEY_CORE][KEY_FRAMEWORK_VERSION]) + "-p1",
+            )
+
     if CORE.using_native_idf:
         # Try to convert PlatformIO library to ESP-IDF components
         for name, library in CORE.platformio_libraries.items():
+            # Don't process arduino libraries
+            if name in ARDUINO_DISABLED_LIBRARIES:
+                continue
             dependency_name, dependency = _platformio_library_to_dependency(library)
             dependencies[dependency_name] = dependency
 
